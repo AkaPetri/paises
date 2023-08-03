c756b242f59090535063ca8082896afa3caf971d
AkaPetri <joaoaugustopetri@gmail.com>
2023-08-03 15:29:26 -0300

    Adicao de arquivos

---
+++ paises.js
@@ -0,0 +1,36 @@
+function buscaPaises() {
+    var url = "https://restcountries.com/v3.1/all";
+    fetch(url)
+      .then((data) => data.json())
+      .then((paises) => {
+        // Sort the countries by name
+        console.log(paises);
+        paises.sort((a, b) => a.name.common.localeCompare(b.name.common));
+        var html = "";
+  
+        for (let i = 0; i < paises.length; i++) {
+          const pais = paises[i];
+  
+          var card_padrao = `
+            <div class="col-4 mb-3">
+              <div class="card" style="width: 18rem;">
+                <img src="${pais.flags.png}" class="card-img-top" alt="Bandeira de ${pais.name.common}">
+                <div class="card-body">
+                  <h5 class="card-title">${pais.translations.por.common}</h5>
+                  <p class="card-text">Continente: ${pais.continents[0]}</p>
+                </div>
+              </div>
+            </div>
+          `;
+  
+          html += card_padrao;
+        }
+  
+        document.getElementById("linha").innerHTML = html;
+      });
+  
+    console.log(url);
+  }
+  
+  buscaPaises();
+  
